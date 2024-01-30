95ffeebc6b0958fd42e7319a154867d82c13d1fb
NymannKMD <59576973+NymannKMD@users.noreply.github.com>
2024-01-30 20:43:18 +0100

    Update install-git-hook.sh

--- install-git-hook.sh
+++ install-git-hook.sh
@@ -12,7 +12,7 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' ERR
 outFile="./resharper-cli.tar.gz"
 gitResharperFolder="./.git/hooks/resharper"
 preCommitFile="./.git/hooks/pre-commit"
-cliUrl="https://download-cf.jetbrains.com/resharper/ReSharperUltimate.2019.3.1/JetBrains.ReSharper.CommandLineTools.Unix.2019.3.1.tar.gz"
+cliUrl="https://download.jetbrains.com/resharper/dotUltimate.2023.3.3/JetBrains.ReSharper.CommandLineTools.2023.3.3.zip?_gl=1*hi8i2b*_ga*MTg2OTE0MzMzNi4xNzA2NjQzMjQw*_ga_9J976DJZ68*MTcwNjY0MzI0MC4xLjEuMTcwNjY0MzYzMy40My4wLjA.&_ga=2.37106208.1254910719.1706643247-1869143336.1706643240"
 preCommitHookUrl="https://raw.githubusercontent.com/GeeWee/resharper-pre-commit-hook/master/pre-commit-hook.sh"
 
 
