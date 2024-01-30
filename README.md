a1694522941c35bb25f3be6aa85c3afdaddf8f25
PNJ@kmd.dk <PNJ@kmd.dk>
2024-01-30 21:48:12 +0100

    Changed to new version af cleanupcode

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # resharper-pre-commit-hook
+
 Resharper format pre-commit hook. It uses resharper to format all the files you're committing, and automatically restages them.
 
 # Installation
+
 Get the latest version using bash, by running this command in the root of your repository:
+
 ```bash
-curl -s https://raw.githubusercontent.com/GeeWee/resharper-pre-commit-hook/master/install-git-hook.sh | bash
+curl -s https://raw.githubusercontent.com/NymannKMD/resharper-pre-commit-hook/master/install-git-hook.sh | bash
 ```
+
 Note that this runs a shell script on your computer. You might want to review the script before running it.
 
 # License
+
 MIT
--- install-git-hook.sh
+++ install-git-hook.sh
@@ -9,21 +9,21 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' ERR
 
-outFile="./resharper-cli.tar.gz"
+outFile="./resharper-cli.zip"
 gitResharperFolder="./.git/hooks/resharper"
 preCommitFile="./.git/hooks/pre-commit"
 cliUrl="https://download.jetbrains.com/resharper/dotUltimate.2023.3.3/JetBrains.ReSharper.CommandLineTools.2023.3.3.zip?_gl=1*hi8i2b*_ga*MTg2OTE0MzMzNi4xNzA2NjQzMjQw*_ga_9J976DJZ68*MTcwNjY0MzI0MC4xLjEuMTcwNjY0MzYzMy40My4wLjA.&_ga=2.37106208.1254910719.1706643247-1869143336.1706643240"
-preCommitHookUrl="https://raw.githubusercontent.com/GeeWee/resharper-pre-commit-hook/master/pre-commit-hook.sh"
+preCommitHookUrl="https://raw.githubusercontent.com/NymannKMD/resharper-pre-commit-hook/master/pre-commit-hook.sh"
 
 
 echo "Fetching Resharper CLI tools"
-curl ${cliUrl} > ${outFile}
+curl -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36" "${cliUrl}" > ${outFile}
 
 echo "Cleaning up old versions"
 rm -rf ${gitResharperFolder} # Delete any old versions
 mkdir -p ${gitResharperFolder}
 echo "Extracting into ${gitResharperFolder}"
-tar -xf "./${outFile}" -C ${gitResharperFolder}
+unzip -o "./${outFile}" -d ${gitResharperFolder}
 
 echo "Adding pre-commit hook"
 curl -s ${preCommitHookUrl} > ${preCommitFile}
